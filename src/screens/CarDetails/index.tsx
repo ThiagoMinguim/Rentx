36991ee8b9d6080b02dc876c063237a743ba35ef
Thiago Minguim <Chazy249@gmail.com>
2022-01-11 10:35:31 -0300

    finish CarDetail

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import { Accessory } from '../../components/Accessory'
 import { BackButton } from '../../components/BackButton'
 import { ImageSlider } from '../../components/ImageSlider'
+
+import speedSvg from '../../assets/speed.svg'
+import accelerationSvg from '../../assets/acceleration.svg'
+import forceSvg from '../../assets/force.svg'
+import gasolineSvg from '../../assets/gasoline.svg'
+import exchangeSvg from '../../assets/exchange.svg'
+import peopleSvg from '../../assets/people.svg'
 
 import * as S from './styles'
 
@@ -32,6 +39,15 @@ export function CarDetails() {
             <S.Price>R$ 560</S.Price>
           </S.Rent>
         </S.Details>
+
+        <S.Acessories>
+          <Accessory name="380km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 hp" icon={forceSvg} />
+          <Accessory name="Gasolina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 pessoas" icon={peopleSvg} />
+        </S.Acessories>
 
         <S.About>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo rem
