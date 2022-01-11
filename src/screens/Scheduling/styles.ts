7f21812390d3bafa83a6620e8e806c3f60730b34
Thiago Minguim <Chazy249@gmail.com>
2022-01-11 14:39:42 -0300

    Scheduling component

---
+++ src/screens/Scheduling/styles.ts
@@ -0,0 +1,69 @@
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { RFValue } from 'react-native-responsive-fontsize'
+import styled, { css } from 'styled-components/native'
+
+interface DateValueProps {
+  selected: boolean
+}
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${props => props.theme.colors.background_secundary};
+`
+
+export const Header = styled.View`
+  width: 100%;
+  height:325px
+
+  background-color: ${props => props.theme.colors.header};
+
+  justify-content: center;
+  padding:25px;
+  padding-top: ${getStatusBarHeight() + 30}px;
+`
+
+export const Title = styled.Text`
+  color: ${props => props.theme.colors.shape};
+  font-family: ${props => props.theme.font.secundary_600};
+  font-size: ${RFValue(24)}px;
+
+  margin-top: 24px;
+`
+export const RentalPeriod = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+export const DateInfo = styled.View`
+  width: 30%;
+`
+export const DateTitle = styled.Text`
+  color: ${props => props.theme.colors.text};
+  font-family: ${props => props.theme.font.secundary_500};
+  font-size: ${RFValue(10)}px;
+`
+export const DateValue = styled.Text<DateValueProps>`
+  color: ${props => props.theme.colors.shape};
+  font-family: ${props => props.theme.font.primary_500};
+  font-size: ${RFValue(14)}px;
+
+  ${({ selected, theme }) =>
+    !selected &&
+    css`
+      border-bottom-width: 1px;
+      border-bottom-color: ${theme.colors.text};
+      padding-bottom: 5px;
+    `}
+`
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    paddingBottom: 24
+  },
+  showsVerticalScrollIndicator: false
+})``
+
+export const Footer = styled.View`
+  padding: 24px;
+`
