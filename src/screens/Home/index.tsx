533c1afd4c935518dd06f61a205f64bf90597164
Thiago Minguim <Chazy249@gmail.com>
2022-01-12 17:07:11 -0300

    Component Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 import { StatusBar } from 'react-native'
 import { RFValue } from 'react-native-responsive-fontsize'
 
@@ -9,6 +10,8 @@ import { Car } from '../../components/Car'
 import * as S from './styles'
 
 export function Home() {
+  const navigation = useNavigation()
+
   const carData = {
     brand: 'Fiat',
     name: 'Uno',
@@ -18,6 +21,10 @@ export function Home() {
     },
     thumbnail:
       'https://e7.pngegg.com/pngimages/895/775/png-clipart-2010-audi-a5-car-audi-coupe-gt-2011-audi-a5-audi-sedan-car-thumbnail.png'
+  }
+
+  function handleCarDetails() {
+    navigation.navigate('CarDetails')
   }
 
   return (
@@ -36,7 +43,7 @@ export function Home() {
       <S.CarList
         data={[1, 2, 3, 4, 5, 6, 7]}
         keyExtractor={item => String(item)}
-        renderItem={() => <Car data={carData} />}
+        renderItem={() => <Car data={carData} onPress={handleCarDetails} />}
       />
     </S.Container>
   )
