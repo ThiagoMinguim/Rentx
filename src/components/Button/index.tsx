c8dbc9435def160c5400c90d43447bcefdf6bcca
Thiago Minguim <Chazy249@gmail.com>
2022-01-12 17:07:23 -0300

    Component Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,11 +5,12 @@ import * as S from './styles'
 interface Props {
   title: string
   color?: string
+  onPress?: () => void
 }
 
-export function Button({ title, color, ...rest }: Props) {
+export function Button({ title, color, onPress, ...rest }: Props) {
   return (
-    <S.Container {...rest} color={color}>
+    <S.Container {...rest} color={color} onPress={onPress}>
       <S.Title> {title} </S.Title>
     </S.Container>
   )
