d049e0e024c23645b9427eb0afb0bba4f01e2365
Thiago Minguim <Chazy249@gmail.com>
2022-01-13 15:58:48 -0300

    WIP

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,79 +3,68 @@ import { Accessory } from '../../components/Accessory'
 import { BackButton } from '../../components/BackButton'
 import { ImageSlider } from '../../components/ImageSlider'
 
-import speedSvg from '../../assets/speed.svg'
-import accelerationSvg from '../../assets/acceleration.svg'
-import forceSvg from '../../assets/force.svg'
-import gasolineSvg from '../../assets/gasoline.svg'
-import exchangeSvg from '../../assets/exchange.svg'
-import peopleSvg from '../../assets/people.svg'
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon'
 
 import * as S from './styles'
 import { Button } from '../../components/Button'
 import { StatusBar } from 'expo-status-bar'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
+
+import { CarDTO } from '../../dtos/CarDTO'
+
+interface Params {
+  car: CarDTO
+}
 
 export function CarDetails() {
+  const route = useRoute()
   const navigation = useNavigation()
 
   function handleConfirmRental() {
     navigation.navigate('Scheduling')
   }
 
+  function handleBack() {
+    navigation.goBack()
+  }
+
+  const { car } = route.params as Params
+
   return (
     <S.Container>
       <StatusBar />
       <S.Header>
-        <BackButton onPress={() => {}} />
+        <BackButton onPress={handleBack} />
       </S.Header>
 
       <S.CarImages>
-        <ImageSlider
-          imagesUrl={[
-            'https://e7.pngegg.com/pngimages/895/775/png-clipart-2010-audi-a5-car-audi-coupe-gt-2011-audi-a5-audi-sedan-car-thumbnail.png'
-          ]}
-        />
+        <ImageSlider imagesUrl={car.photos} />
       </S.CarImages>
 
       <S.Content>
         <S.Details>
           <S.Description>
-            <S.Brand>Lamborghini</S.Brand>
-            <S.Name>Huracan</S.Name>
+            <S.Brand>{car.brand}</S.Brand>
+            <S.Name>{car.name}</S.Name>
           </S.Description>
 
           <S.Rent>
-            <S.Period>Ao dia</S.Period>
-            <S.Price>R$ 560</S.Price>
+            <S.Period>{car.rent.period}</S.Period>
+            <S.Price>{car.rent.price}</S.Price>
           </S.Rent>
         </S.Details>
 
         <S.Acessories>
-          <Accessory name="380km/h" icon={speedSvg} />
-          <Accessory name="3.2s" icon={accelerationSvg} />
-          <Accessory name="800 hp" icon={forceSvg} />
-          <Accessory name="Gasolina" icon={gasolineSvg} />
-          <Accessory name="Auto" icon={exchangeSvg} />
-          <Accessory name="2 pessoas" icon={peopleSvg} />
+          {car.accessories.map(accessory => (
+            <Accessory
+              key={accessory.type}
+              name={accessory.name}
+              icon={getAccessoryIcon(accessory.type)}
+            />
+          ))}
         </S.Acessories>
 
-        <S.About>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo rem
-          labore sunt eum, commodi, velit ducimus vero, corporis veniam quam
-          ratione impedit nihil. Quisquam dicta temporibus doloribus,laborum.
-        </S.About>
-
-        <S.About>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo rem
-          labore sunt eum, commodi, velit ducimus vero, corporis veniam quam
-          ratione impedit nihil. Quisquam dicta temporibus doloribus,laborum.
-        </S.About>
-
-        <S.About>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo rem
-          labore sunt eum, commodi, velit ducimus vero, corporis veniam quam
-          ratione impedit nihil. Quisquam dicta temporibus doloribus,laborum.
-        </S.About>
+        <S.About>{car.about}</S.About>
       </S.Content>
 
       <S.Footer>
