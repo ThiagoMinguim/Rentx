d51b2067ca6e76dda5d6a0d2333df0f4a6ff5b41
Thiago Minguim <Chazy249@gmail.com>
2022-01-17 14:44:55 -0300

    Changes In Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
+import { ActivityIndicator } from 'react-native'
+import { TouchableOpacityProps } from 'react-native'
+import theme from '../../styles/theme'
 
 import * as S from './styles'
 
-interface Props {
+interface Props extends TouchableOpacityProps {
   title: string
   color?: string
   onPress?: () => void
+  disabled?: boolean
+  loading?: boolean
 }
 
-export function Button({ title, color, onPress, ...rest }: Props) {
+export function Button({
+  title,
+  color,
+  onPress,
+  disabled = false,
+  loading = false
+}: Props) {
   return (
-    <S.Container {...rest} color={color} onPress={onPress}>
-      <S.Title> {title} </S.Title>
+    <S.Container
+      color={color}
+      onPress={onPress}
+      disabled={disabled}
+      style={{ opacity: disabled === true || loading === true ? 0.5 : 1 }}>
+      {loading ? (
+        <ActivityIndicator color={theme.colors.shape} />
+      ) : (
+        <S.Title> {title} </S.Title>
+      )}
     </S.Container>
   )
 }
