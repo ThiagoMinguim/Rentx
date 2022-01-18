3e3d325089d26af8527758aaecb16247196de759
Thiago Minguim <Chazy249@gmail.com>
2022-01-18 15:45:11 -0300

    slider

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { FlatList, ViewToken } from 'react-native'
 
 import * as S from './styles'
 
@@ -6,19 +7,39 @@ interface Props {
   imagesUrl: string[]
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[]
+  changed: ViewToken[]
+}
+
 export function ImageSlider({ imagesUrl }: Props) {
+  const [imageIndex, setImageIndex] = useState(0)
+
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    const index = info.viewableItems[0].index!
+    setImageIndex(index)
+  })
+
   return (
     <S.Container>
       <S.ImageIndexes>
-        <S.ImageIndex active={true} />
-        <S.ImageIndex active={false} />
-        <S.ImageIndex active={false} />
-        <S.ImageIndex active={false} />
+        {imagesUrl.map((_, index) => (
+          <S.ImageIndex key={String(index)} active={index === imageIndex} />
+        ))}
       </S.ImageIndexes>
 
-      <S.CardImageWrapper>
-        <S.CardImage source={{ uri: imagesUrl[0] }} resizeMode="contain" />
-      </S.CardImageWrapper>
+      <FlatList
+        data={imagesUrl}
+        keyExtractor={key => key}
+        renderItem={({ item }) => (
+          <S.CardImageWrapper>
+            <S.CardImage source={{ uri: item }} />
+          </S.CardImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={indexChanged}
+      />
     </S.Container>
   )
 }
