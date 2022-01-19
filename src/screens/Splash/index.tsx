13dc6e86c46ca82351479098bf727ddf751249c9
Thiago Minguim <Chazy249@gmail.com>
2022-01-19 10:37:05 -0300

    Splash index

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { Button, StyleSheet } from 'react-native'
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle
+} from 'react-native-reanimated'
 
 import * as S from './styles'
 
 export function Splash() {
-  return <S.Container>
-    
-  </S.Container>
+  const animation = useSharedValue(0)
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: animation.value
+        }
+      ]
+    }
+  })
+
+  function handlePositionAnimation() {
+    animation.value = Math.random() * 400
+  }
+
+  return (
+    <S.Container>
+      <Animated.View style={[styles.box, animatedStyles]} />
+
+      <Button title="Mover" onPress={handlePositionAnimation} />
+    </S.Container>
+  )
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  }
+})
