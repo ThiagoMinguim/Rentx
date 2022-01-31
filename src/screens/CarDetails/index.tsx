0ea69d5d1f58c36b2e3c486708f79427d6c139ee
Thiago Minguim <Chazy249@gmail.com>
2022-01-31 14:51:52 -0300

    FIX: Remove console.log

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -40,7 +40,6 @@ export function CarDetails({ navigation, route }: CarDetailsProps) {
 
   const scrollHandler = useAnimatedScrollHandler(event => {
     scrollY.value = event.contentOffset.y
-    console.log(event.contentOffset.y)
   })
 
   const headerStyleAnimation = useAnimatedStyle(() => {
